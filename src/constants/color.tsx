1bf79f3d07752669f177a2bb2ae2d2307bc41bd9
0xBono <0xbono@naver.com>
2020-04-10 00:50:46 +0900

    chore(color): Color definition

---
+++ src/constants/color.tsx
@@ -0,0 +1,11 @@
+const Color = {
+  Purple: '#7544F7',
+  Black: '#1F1F1F',
+  Gray: '#555555',
+  Orange: '#FB6C37',
+  White: '#FFFFFF',
+  DarkShadow: 'rgba(31, 31, 31, 0.4)',
+  PurpleShadow: 'rgba(124, 76, 250, 0.4)',
+};
+
+export default Color;
