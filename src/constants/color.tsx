9fe5e6642e0fd59b377a96a07bef05821f1cef27
0xBono <0xbono@naver.com>
2020-04-10 17:51:39 +0900

    chore: updated color

--- src/constants/color.tsx
+++ src/constants/color.tsx
@@ -3,6 +3,8 @@ const Color = {
   Black: '#1F1F1F',
   Gray: '#555555',
   Orange: '#FB6C37',
+  Yellow: '#FFE600',
+  Brown: '#3D1E1F',
   White: '#FFFFFF',
   DarkShadow: 'rgba(31, 31, 31, 0.4)',
   PurpleShadow: 'rgba(124, 76, 250, 0.4)',
